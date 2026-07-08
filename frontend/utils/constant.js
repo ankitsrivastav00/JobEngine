9dd92842a5e61a90fc368a17bec97969025e3c44
Ankit Srivastav <ankitshrivastav0008@gmail.com>
2026-07-08 12:30:21 +0530

    Use environment variable for API URL

--- frontend/utils/constant.js
+++ frontend/utils/constant.js
@@ -1,10 +1,6 @@
-// export const USER_API_END_POINT="https://jobwebsite-youtube-1.onrender.com/api/v1/user";
-// export const JOB_API_END_POINT="https://jobwebsite-youtube-1.onrender.com/api/v1/job";
-// export const Application_API_END_POINT="https://jobwebsite-youtube-1.onrender.com/api/v1/application";
-// export const Company_API_END_POINT="https://jobwebsite-youtube-1.onrender.com/api/v1/company";
+const BASE_URL = import.meta.env.VITE_API_URL;
 
-
-export const USER_API_END_POINT="http://localhost:5000/api/v1/user";
-export const JOB_API_END_POINT="http://localhost:5000/api/v1/job";
-export const Application_API_END_POINT="http://localhost:5000/api/v1/application";
-export const Company_API_END_POINT="http://localhost:5000/api/v1/company";
+export const USER_API_END_POINT = `${BASE_URL}/api/v1/user`;
+export const JOB_API_END_POINT = `${BASE_URL}/api/v1/job`;
+export const Application_API_END_POINT = `${BASE_URL}/api/v1/application`;
+export const Company_API_END_POINT = `${BASE_URL}/api/v1/company`;
